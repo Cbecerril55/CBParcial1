71bb7f45ab4693d71dcc2ae2b7aa429751461234
Cbecerril55 <113248117+Cbecerril55@users.noreply.github.com>
2023-03-02 19:20:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { sueldoD  } = require('./meses/sueldo');
 const { esVocal  } = require('./vocales/vocal');
+const { numeroMenor  } = require('./menores/menores');
 
 console.log("El sueldo neto "+ sueldoD(215));
 console.log(esVocal("a"));
-console.log("prueba");
+console.log(numeroMenor(1,8,6));
